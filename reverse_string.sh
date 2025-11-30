d01c3036d91bba4a1a312d61f425334fdcb78962
HarshithNA <135431656+HarshithNA@users.noreply.github.com>
2025-11-30 17:29:05 +0530

    Update reverse_string.sh

--- reverse_string.sh
+++ reverse_string.sh
@@ -4,5 +4,5 @@ string="Harshith"
 echo "The given string is $string"
 echo "The reverse of the given string is $(echo $string | rev)"
 echo "time stamp $(date)" >> log
-echo "adding a new line"
-echo "This is assignment"
+echo "Today is sunday"
+
